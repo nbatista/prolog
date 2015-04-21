18071c2e19629e7288790ea12ae03c6f19c83bed
nbatista <nbatista@inf.ufsm.br>
2015-04-21 13:49:51 -0300

    local

---
+++ t4-assassinato.pl
@@ -0,0 +1,47 @@
+local(pedro, santa maria, segunda).
+local(pedro, santa maria, terca).
+local(pedro, porto alegre, quarta).
+local(pedro, santa maria, quinta).
+local(pedro, apartamento, sexta).
+
+local(caren, porto alegre, segunda).
+local(caren, porto alegre, terca).
+local(caren, porto alegre, quarta).
+local(caren, santa maria, quinta).
+local(caren, apartamento, sexta).
+
+local(henrique, apartamento, segunda).
+local(henrique, porto alegre, terca).
+local(henrique, apartamento, quarta).
+local(henrique, apartamento, quinta).
+local(henrique, apartamento, sexta).
+
+local(bia, apartamento, segunda).
+local(bia, porto alegre, terca).
+local(bia, porto alegre, quarta).
+local(bia, santa maria, quinta).
+local(bia, apartamento, sexta).
+
+local(adriano, apartamento, segunda).
+local(adriano, apartamento, terca).
+local(adriano, santa maria, quarta).
+local(adriano, apartamento, quinta).
+local(adriano, apartamento, sexta).
+
+local(alice, apartamento, segunda).
+local(alice, porto alegre, terca).
+local(alice, porto alegre, quarta).
+local(alice, apartamento, quinta).
+local(alice, apartamento, sexta).
+
+local(bernardo, santa maria, segunda).
+local(bernardo, santa maria, terca).
+local(bernardo, porto alegre, quarta).
+local(bernardo, santa maria, quinta).
+local(bernardo, apartamento, sexta).
+
+local(maria, apartamento, segunda).
+local(maria, santa maria, terca).
+local(maria, santa maria, quarta).
+local(maria, santa maria, quinta).
+local(maria, apartamento, sexta).
